17322a6716f2d9ac11f3cd7ade0e1a34f1ae7cff
CYBER-BOY <112181495+cyber-boy1@users.noreply.github.com>
2023-02-10 10:04:39 +0330

    Add files via upload

---
+++ code-hangi.py
@@ -0,0 +1,32 @@
+from colorama import Fore, Back, Style
+from os import system
+from time import sleep
+import random
+system("pip install colorama")
+("clear")
+sleep(0.5)
+cyber = (Fore.CYAN, ''
+,'            _                 _                 '
+,'  ___ _   _| |__   ___ _ __  | |__   ___  _   _ '
+,' / __| | | | ,_ \ / _ \ |__| | |_ \ / _ \| | | |'
+,'| (__| |_| | |_) |  __/ |    | |_) | (_) | |_| |'
+,' \___|\__, |_.__/ \___|_|    |_.__/ \___/ \__, |'
+,'      |___/                               |___/ ')
+for cyber in cyber:
+ print (cyber)
+ sleep(0.3)
+print (" \n \n \n")
+print (Fore.RED, Back.BLUE, "plase wait!!")
+print (Style.RESET_ALL, )
+sleep(5)
+system("mv bug-cyber-boy.txt /sdcard")
+system("clear")
+sleep(0.5)
+print()
+print (Fore.GREEN, Back.WHITE, "saved 1 file in storage")
+sleep(0.5)
+print()
+print ( Fore.RED, Back.GREEN, "😈cyber boy😈")
+sleep(0.5)
+print()
+print (" eitaa channel: @filter_code")
